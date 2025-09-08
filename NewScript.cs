22ee33af24c0b4149807238c7c0ad1ed412fcc81
Alessandro Costa <alessandrocnas@proton.me>
2025-09-07 23:10:37 -0300

    add launch.json and tasks.json

--- NewScript.cs
+++ NewScript.cs
@@ -1,4 +1,18 @@
 using System;
 using Godot;
 
-public partial class NewScript : Node { }
+public partial class NewScript : Node
+{
+    [Export]
+    public int PropiedadManeira { get; set; }
+
+    public override void _Ready()
+    {
+        GD.Print("Node is ready!");
+    }
+
+    public override void _Process(double delta)
+    {
+        GD.Print("_Process!");
+    }
+}
